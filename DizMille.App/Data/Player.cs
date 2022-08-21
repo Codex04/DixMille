a7f85dd6d976aaea0abb148f6f41b59768485380
Quentin Metge <quentin.metge@hotmail.fr>
2022-08-21 14:51:05 +0200

    Initialize Repo

---
+++ DizMille.App/Data/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace DizMille.App.Data
+{
+    public class Player
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int Score { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
